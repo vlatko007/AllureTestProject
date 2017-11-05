5d50d497bef2539a10f0b9c579ff272001e45d57
Vlatko <Vlatko@DESKTOP-BOP8FHE>
2017-11-05 09:17:38 +0100

    update driver

--- src/test/java/com/example/allure/tests/Tests.java
+++ src/test/java/com/example/allure/tests/Tests.java
@@ -5,30 +5,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.example.allure.wedoqablog.WedoqaBlogPage;
+import java.io.File;
+import org.openqa.selenium.chrome.ChromeDriverService;
 
 public class Tests {
 
-	@Test
-	public void printPosts() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		WedoqaBlogPage wedoqaBlogPage = new WedoqaBlogPage(driver);
-		wedoqaBlogPage.getPosts().stream()
-			//.filter(postPanel -> postPanel.getAuthor().equals("Tamás Kovacsics"))
-			.forEach(postPanel -> {
-					System.out.println("Title: " + postPanel.getTitle());
-					System.out.println("Author: " + postPanel.getAuthor());
-					//System.out.println("Content: " + postPanel.getContent());
-				});
-                
-                wedoqaBlogPage.clickFirstPost();
-                
-                driver.navigate().back();
-                                
-                wedoqaBlogPage.clickFirstPost();
-                
-                driver.navigate().back();
-                
-		driver.quit();
-	}
+    String driverLocation = new File("./src/resources/driver/chromedriver.exe")
+            .getAbsolutePath();
+
+    @Test
+    public void printPosts() throws InterruptedException {
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, driverLocation);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WedoqaBlogPage wedoqaBlogPage = new WedoqaBlogPage(driver);
+        wedoqaBlogPage.getPosts().stream()
+                //.filter(postPanel -> postPanel.getAuthor().equals("Tamás Kovacsics"))
+                .forEach(postPanel -> {
+                    System.out.println("Title: " + postPanel.getTitle());
+                    System.out.println("Author: " + postPanel.getAuthor());
+                    //System.out.println("Content: " + postPanel.getContent());
+                });
+
+        wedoqaBlogPage.clickFirstPost();
+
+        driver.navigate().back();
+
+        wedoqaBlogPage.clickFirstPost();
+
+        driver.navigate().back();
+
+        driver.quit();
+    }
 }
